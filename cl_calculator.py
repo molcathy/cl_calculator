d9971d6faf07d3a727963de3fe8d7a505fd7180e
mol-george_gm-mbp <mol.george@gmail.com>
2021-07-11 01:24:58 +0100

    modified imports to import specific modules from tkinter; configured cl_calculator.py to work as a module

--- cl_calculator.py
+++ cl_calculator.py
@@ -1,9 +1,9 @@
 #from teacher_programme import draw_axes
-from tkinter import *
+from tkinter import Tk, Canvas, mainloop
 
 
 def fixed_scale_values(start_choice, end_choice, real_start, real_end):
-    # figures out the gradient (m) and the y intercept (c)
+    """Figures out the gradient (m) and the y intercept (c)."""
     real_difference = real_end - real_start
     choice_difference = end_choice - start_choice
     m = real_difference / choice_difference
@@ -12,26 +12,28 @@ def fixed_scale_values(start_choice, end_choice, real_start, real_end):
 
 
 def y_scale(y, start_y_choice, end_y_choice, real_y_start, real_y_end):
-    # figures out the actual value of y, using the gradient and y intercept, but also y * -1, because  it needs to start from the bottom as it goes upwards for a positive gradient
+    """Figures out the actual value of y, using the gradient and y
+    intercept, but also y * -1, because  it needs to start from the bottom as it goes upwards for a positive gradient."""
     y_m, y_c = fixed_scale_values(
         start_y_choice, end_y_choice, real_y_start, real_y_end)
     return (y * -1) * y_m + y_c
 
 
 def x_scale(x, start_x_choice, end_x_choice, real_x_start, real_x_end):
-    # this figures out the actual value of x
+    """This figures out the actual value of x."""
     x_m, x_c = fixed_scale_values(
         start_x_choice, end_x_choice, real_x_start, real_x_end)
     return x * x_m + x_c
 
 
 def draw_lines(x1, x2, y1, y2, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end):
+    """"""
     canvas.create_line(x_scale(x1, start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(y1, start_y_choice, end_y_choice, real_y_start, real_y_end), x_scale(
         x2, start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(y2, start_y_choice, end_y_choice, real_y_start, real_y_end), width=2, fill="green")
 
 
 def indent(x1, x2, y1, y2, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end):
-    # this creates indentations along the x and y axis lines with the values of the indents
+    """This creates indentations along the x and y axis lines with the values of the indents."""
     if int(y1) == 0:
         for x in range(x1, (x2 + 1)):
             draw_lines(x, x, 0, -0.3, start_x_choice, end_x_choice, real_x_start,
@@ -48,7 +50,7 @@ def indent(x1, x2, y1, y2, start_x_choice, end_x_choice, real_x_start, real_x_en
 
 
 def power2_curve(a, b, c, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end):
-    # this creates a curve made by a polynomial to the power of 2
+    """This creates a curve made by a polynomial to the power of 2."""
     new_x = []
     new_y = []
     max = 0
@@ -65,7 +67,7 @@ def power2_curve(a, b, c, start_x_choice, end_x_choice, real_x_start, real_x_end
 
 
 def line(m, c, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end):
-    # this creates a line
+    """This creates a line."""
     list_x = []
     list_y = []
 
@@ -84,40 +86,40 @@ def line(m, c, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_c
             list_x[i + 1], start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(list_y[i + 1], start_y_choice, end_y_choice, real_y_start, real_y_end), width=2, fill="black")
 
 
-root = Tk()
+if __name__ == "__main__":
+    root = Tk()
 
-canvas = Canvas(width=1000, height=1000, bg="lightblue")
-canvas.grid()
+    canvas = Canvas(width=1000, height=1000, bg="lightblue")
+    canvas.grid()
 
-real_x_start = 100
-real_x_end = 700
-start_x_choice = -5
-end_x_choice = 5
+    real_x_start = 100
+    real_x_end = 700
+    start_x_choice = -5
+    end_x_choice = 5
 
-real_y_start = 100
-real_y_end = 700
-start_y_choice = -5
-end_y_choice = 5
+    real_y_start = 100
+    real_y_end = 700
+    start_y_choice = -5
+    end_y_choice = 5
 
-x_m, x_c = fixed_scale_values(
-    start_x_choice, end_x_choice, real_x_start, real_x_end)
-y_m, y_c = fixed_scale_values(
-    start_y_choice, end_y_choice, real_y_start, real_y_end)
+    x_m, x_c = fixed_scale_values(
+        start_x_choice, end_x_choice, real_x_start, real_x_end)
+    y_m, y_c = fixed_scale_values(
+        start_y_choice, end_y_choice, real_y_start, real_y_end)
 
-draw_lines(start_x_choice, end_x_choice, 0, 0, start_x_choice, end_x_choice,
+    draw_lines(start_x_choice, end_x_choice, 0, 0, start_x_choice, end_x_choice,
+               real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
+    draw_lines(0, 0, start_y_choice, end_y_choice, start_x_choice, end_x_choice,
+               real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
+
+    indent(start_x_choice, end_x_choice, 0, 0, start_x_choice, end_x_choice,
            real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
-draw_lines(0, 0, start_y_choice, end_y_choice, start_x_choice, end_x_choice,
+    indent(0, 0, start_y_choice, end_y_choice, start_x_choice, end_x_choice,
            real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
 
-indent(start_x_choice, end_x_choice, 0, 0, start_x_choice, end_x_choice,
-       real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
-indent(0, 0, start_y_choice, end_y_choice, start_x_choice, end_x_choice,
-       real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
+    power2_curve(3, 4, 2, start_x_choice, end_x_choice, real_x_start,
+                 real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
+    line(1, 8, start_x_choice, end_x_choice, real_x_start, real_x_end,
+         start_y_choice, end_y_choice, real_y_start, real_y_end)
 
-power2_curve(3, 4, 2, start_x_choice, end_x_choice, real_x_start,
-             real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
-line(1, 8, start_x_choice, end_x_choice, real_x_start, real_x_end,
-     start_y_choice, end_y_choice, real_y_start, real_y_end)
-
-
-mainloop()
+    mainloop()
