de902145d6cc506d0ded92f7df79d1059cb5174a
george m <mol.george@gmail.com>
2021-07-13 21:37:48 +0100

    refactored code gm

--- cl_calculator.py
+++ cl_calculator.py
@@ -1,4 +1,4 @@
-#from teacher_programme import draw_axes
+# from teacher_programme import draw_axes
 from tkinter import Tk, Canvas, mainloop
 
 
@@ -15,58 +15,157 @@ def y_scale(y, start_y_choice, end_y_choice, real_y_start, real_y_end):
     """Figures out the actual value of y, using the gradient and y
     intercept, but also y * -1, because  it needs to start from the bottom as it goes upwards for a positive gradient."""
     y_m, y_c = fixed_scale_values(
-        start_y_choice, end_y_choice, real_y_start, real_y_end)
+        start_y_choice, end_y_choice, real_y_start, real_y_end
+    )
     return (y * -1) * y_m + y_c
 
 
 def x_scale(x, start_x_choice, end_x_choice, real_x_start, real_x_end):
     """This figures out the actual value of x."""
     x_m, x_c = fixed_scale_values(
-        start_x_choice, end_x_choice, real_x_start, real_x_end)
+        start_x_choice, end_x_choice, real_x_start, real_x_end
+    )
     return x * x_m + x_c
 
 
-def draw_lines(x1, x2, y1, y2, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end):
+def draw_lines(
+    x1,
+    x2,
+    y1,
+    y2,
+    start_x_choice,
+    end_x_choice,
+    real_x_start,
+    real_x_end,
+    start_y_choice,
+    end_y_choice,
+    real_y_start,
+    real_y_end,
+):
     """"""
-    canvas.create_line(x_scale(x1, start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(y1, start_y_choice, end_y_choice, real_y_start, real_y_end), x_scale(
-        x2, start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(y2, start_y_choice, end_y_choice, real_y_start, real_y_end), width=2, fill="green")
+    canvas.create_line(
+        x_scale(x1, start_x_choice, end_x_choice, real_x_start, real_x_end),
+        y_scale(y1, start_y_choice, end_y_choice, real_y_start, real_y_end),
+        x_scale(x2, start_x_choice, end_x_choice, real_x_start, real_x_end),
+        y_scale(y2, start_y_choice, end_y_choice, real_y_start, real_y_end),
+        width=2,
+        fill="green",
+    )
 
 
-def indent(x1, x2, y1, y2, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end):
+def indent(
+    x1,
+    x2,
+    y1,
+    y2,
+    start_x_choice,
+    end_x_choice,
+    real_x_start,
+    real_x_end,
+    start_y_choice,
+    end_y_choice,
+    real_y_start,
+    real_y_end,
+):
     """This creates indentations along the x and y axis lines with the values of the indents."""
     if int(y1) == 0:
         for x in range(x1, (x2 + 1)):
-            draw_lines(x, x, 0, -0.3, start_x_choice, end_x_choice, real_x_start,
-                       real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
-            canvas.create_text(x_scale(x, start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(
-                -0.5, start_y_choice, end_y_choice, real_y_start, real_y_end), text=str(x), fill="green")
+            draw_lines(
+                x,
+                x,
+                0,
+                -0.3,
+                start_x_choice,
+                end_x_choice,
+                real_x_start,
+                real_x_end,
+                start_y_choice,
+                end_y_choice,
+                real_y_start,
+                real_y_end,
+            )
+            canvas.create_text(
+                x_scale(x, start_x_choice, end_x_choice, real_x_start, real_x_end),
+                y_scale(-0.5, start_y_choice, end_y_choice, real_y_start, real_y_end),
+                text=str(x),
+                fill="green",
+            )
 
     if int(x1) == 0:
         for y in range(y1, (y2 + 1)):
-            draw_lines(0, -0.15, y, y, start_x_choice, end_x_choice, real_x_start,
-                       real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
-            canvas.create_text(x_scale(-0.5, start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(
-                y, start_y_choice, end_y_choice, real_y_start, real_y_end), text=str(y), fill="green")
+            draw_lines(
+                0,
+                -0.15,
+                y,
+                y,
+                start_x_choice,
+                end_x_choice,
+                real_x_start,
+                real_x_end,
+                start_y_choice,
+                end_y_choice,
+                real_y_start,
+                real_y_end,
+            )
+            canvas.create_text(
+                x_scale(-0.5, start_x_choice, end_x_choice, real_x_start, real_x_end),
+                y_scale(y, start_y_choice, end_y_choice, real_y_start, real_y_end),
+                text=str(y),
+                fill="green",
+            )
 
 
-def power2_curve(a, b, c, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end):
+def power2_curve(
+    a,
+    b,
+    c,
+    start_x_choice,
+    end_x_choice,
+    real_x_start,
+    real_x_end,
+    start_y_choice,
+    end_y_choice,
+    real_y_start,
+    real_y_end,
+):
     """This creates a curve made by a polynomial to the power of 2."""
     new_x = []
     new_y = []
     max = 0
     x = start_x_choice
     while x <= end_x_choice:
-        y = a * x**2 + b * x + c
+        y = a * x ** 2 + b * x + c
         if y >= start_y_choice and y <= end_y_choice:
             new_x.append(x)
             new_y.append(y)
         x += 0.01  # add a really small amount so it looks like a curve
     for i in range(0, len(new_y) - 1):
-        canvas.create_line(x_scale(new_x[i], start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(new_y[i], start_y_choice, end_y_choice, real_y_start, real_y_end), x_scale(
-            new_x[i + 1], start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(new_y[i + 1], start_y_choice, end_y_choice, real_y_start, real_y_end), width=2, fill="blue")
+        canvas.create_line(
+            x_scale(new_x[i], start_x_choice, end_x_choice, real_x_start, real_x_end),
+            y_scale(new_y[i], start_y_choice, end_y_choice, real_y_start, real_y_end),
+            x_scale(
+                new_x[i + 1], start_x_choice, end_x_choice, real_x_start, real_x_end
+            ),
+            y_scale(
+                new_y[i + 1], start_y_choice, end_y_choice, real_y_start, real_y_end
+            ),
+            width=2,
+            fill="blue",
+        )
 
 
-def line(m, c, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end):
+def line(
+    m,
+    c,
+    start_x_choice,
+    end_x_choice,
+    real_x_start,
+    real_x_end,
+    start_y_choice,
+    end_y_choice,
+    real_y_start,
+    real_y_end,
+):
     """This creates a line."""
     list_x = []
     list_y = []
@@ -82,8 +181,18 @@ def line(m, c, start_x_choice, end_x_choice, real_x_start, real_x_end, start_y_c
         x += 0.1
         # the smaller the value added the more accurate the result and matches the graph
     for i in range(0, len(list_y) - 1):
-        canvas.create_line(x_scale(list_x[i], start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(list_y[i], start_y_choice, end_y_choice, real_y_start, real_y_end), x_scale(
-            list_x[i + 1], start_x_choice, end_x_choice, real_x_start, real_x_end), y_scale(list_y[i + 1], start_y_choice, end_y_choice, real_y_start, real_y_end), width=2, fill="black")
+        canvas.create_line(
+            x_scale(list_x[i], start_x_choice, end_x_choice, real_x_start, real_x_end),
+            y_scale(list_y[i], start_y_choice, end_y_choice, real_y_start, real_y_end),
+            x_scale(
+                list_x[i + 1], start_x_choice, end_x_choice, real_x_start, real_x_end
+            ),
+            y_scale(
+                list_y[i + 1], start_y_choice, end_y_choice, real_y_start, real_y_end
+            ),
+            width=2,
+            fill="black",
+        )
 
 
 if __name__ == "__main__":
@@ -103,23 +212,94 @@ if __name__ == "__main__":
     end_y_choice = 5
 
     x_m, x_c = fixed_scale_values(
-        start_x_choice, end_x_choice, real_x_start, real_x_end)
+        start_x_choice, end_x_choice, real_x_start, real_x_end
+    )
     y_m, y_c = fixed_scale_values(
-        start_y_choice, end_y_choice, real_y_start, real_y_end)
+        start_y_choice, end_y_choice, real_y_start, real_y_end
+    )
 
-    draw_lines(start_x_choice, end_x_choice, 0, 0, start_x_choice, end_x_choice,
-               real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
-    draw_lines(0, 0, start_y_choice, end_y_choice, start_x_choice, end_x_choice,
-               real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
+    draw_lines(
+        start_x_choice,
+        end_x_choice,
+        0,
+        0,
+        start_x_choice,
+        end_x_choice,
+        real_x_start,
+        real_x_end,
+        start_y_choice,
+        end_y_choice,
+        real_y_start,
+        real_y_end,
+    )
+    draw_lines(
+        0,
+        0,
+        start_y_choice,
+        end_y_choice,
+        start_x_choice,
+        end_x_choice,
+        real_x_start,
+        real_x_end,
+        start_y_choice,
+        end_y_choice,
+        real_y_start,
+        real_y_end,
+    )
 
-    indent(start_x_choice, end_x_choice, 0, 0, start_x_choice, end_x_choice,
-           real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
-    indent(0, 0, start_y_choice, end_y_choice, start_x_choice, end_x_choice,
-           real_x_start, real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
+    indent(
+        start_x_choice,
+        end_x_choice,
+        0,
+        0,
+        start_x_choice,
+        end_x_choice,
+        real_x_start,
+        real_x_end,
+        start_y_choice,
+        end_y_choice,
+        real_y_start,
+        real_y_end,
+    )
+    indent(
+        0,
+        0,
+        start_y_choice,
+        end_y_choice,
+        start_x_choice,
+        end_x_choice,
+        real_x_start,
+        real_x_end,
+        start_y_choice,
+        end_y_choice,
+        real_y_start,
+        real_y_end,
+    )
 
-    power2_curve(3, 4, 2, start_x_choice, end_x_choice, real_x_start,
-                 real_x_end, start_y_choice, end_y_choice, real_y_start, real_y_end)
-    line(1, 8, start_x_choice, end_x_choice, real_x_start, real_x_end,
-         start_y_choice, end_y_choice, real_y_start, real_y_end)
+    power2_curve(
+        3,
+        4,
+        2,
+        start_x_choice,
+        end_x_choice,
+        real_x_start,
+        real_x_end,
+        start_y_choice,
+        end_y_choice,
+        real_y_start,
+        real_y_end,
+    )
+    line(
+        1,
+        8,
+        start_x_choice,
+        end_x_choice,
+        real_x_start,
+        real_x_end,
+        start_y_choice,
+        end_y_choice,
+        real_y_start,
+        real_y_end,
+    )
 
     mainloop()
